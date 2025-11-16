4f64827b87c4e7c4e33f7296e806be10f44f8c49
Arunia <charu1234569@gmail.com>
2025-11-16 21:24:04 +0530

    ....

--- Saathi/settings.py
+++ Saathi/settings.py
@@ -20,6 +20,7 @@ load_dotenv()
 password = os.getenv("POSTGRESQL_PASS")
 database_url  = os.getenv("DATABASE_URL")
 allowed_host = os.getenv("ALLOWED_HOSTS").split(',')
+debug = os.getenv("DEBUG")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -32,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_zkjfeh%5zg=ij99sana#t*c=2pbv+xtlz3!x7!u90^1sh(w*3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = debug
 
 ALLOWED_HOSTS = [allowed_host]
 
